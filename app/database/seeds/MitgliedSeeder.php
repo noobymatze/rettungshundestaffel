563635fde80dbe82c74da04b86150adb2864db98
Matthias Metzger <noobymatze@yahoo.de>
2014-07-06 16:17:49 +0200

    MitgliedSeeder erweitert, Mitglied kommentiert
    Mitglied Model ist nun auf Laravel 4.2 umgestellt, sowie vollstäig kommentiert. Der Seeder fü Datenbank wurde erweitert.

--- app/database/seeds/MitgliedSeeder.php
+++ app/database/seeds/MitgliedSeeder.php
@@ -14,5 +14,37 @@ class MitgliedSeeder extends Seeder {
             'passwort' => Hash::make('peter09')
         ));
 
+        Mitglied::create(array(
+            'vorname' => 'Eugen',
+            'nachname' => 'Feit',
+            'rolle' => 'Staffelleitung',
+            'email' => 'eugen.feit@tester.com',
+            'passwort' => Hash::make('peter09')
+        ));
+
+        Mitglied::create(array(
+            'vorname' => 'Jannik',
+            'nachname' => 'Hell',
+            'rolle' => 'Staffelleitung',
+            'email' => 'jannik.hell@tester.com',
+            'passwort' => Hash::make('peter09')
+        ));
+
+        Mitglied::create(array(
+            'vorname' => 'Barbara',
+            'nachname' => 'Müller',
+            'rolle' => 'Staffelleitung',
+            'email' => 'b.mueller@tesre.com',
+            'passwort' => Hash::make('peter09')
+        ));
+
+        Mitglied::create(array(
+            'vorname' => 'Hallo',
+            'nachname' => 'Welt',
+            'rolle' => 'Staffelleitung',
+            'email' => 'hallo.welt@tester.com',
+            'passwort' => Hash::make('peter09')
+        ));
+
     }
 }

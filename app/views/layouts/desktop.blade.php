cec58d1d2603beaea1b32b421a468df22cd0498f
Eugen Feit <eugen.feit@googlemail.com>
2014-07-06 20:06:06 +0200

    Sidebar verbessert, Links hinzugefügt

--- app/views/layouts/desktop.blade.php
+++ app/views/layouts/desktop.blade.php
@@ -8,45 +8,72 @@
         <div id="wrapper">
             <!-- Sidebar -->
             @section('sidebar')
-                <div id="sidebar-wrapper">
-                    <nav class="sidebar-nav">
-                        <li class="sidebar-brand"><a href="#">Rettungshundestaffel</a>
-                        </li>
-                        <li><a href="#">Dashboard</a>
-                        </li>
-                        <li><a href="#">Mitglieder</a>
-                        </li>
-                        <li><a href="#">Suchgebiete</a>
-                        </li>
-                        <li><a href="#">Termine</a>
-                        </li>
-                    </nav>
-                </div>
-                <script>
-                    window.addEventListener("DOMContentLoaded", function() {
-                        $("#menu-toggle").click(function(e) {
-                            e.preventDefault();
-                            $("#wrapper").toggleClass("active");
-                        });
-                    });
-                </script>
+			<div id="sidebar-wrapper">
+				<nav class="sidebar-nav nav pills">
+					<li class="sidebar-brand">
+						<a href="#">Rettungshundestaffel</a>
+					</li>
+					<li>
+						<a href=""><i class="glyphicon glyphicon-home"></i> Dashboard</a>
+					</li>
+					<li>
+						<a href="mitglieder"><i class="glyphicon glyphicon-user"></i> Mitglieder</a>
+					</li>
+					<li>
+						<a href="suchgebiete"><i class="glyphicon glyphicon-tree-conifer"></i> Suchgebiete</a>
+					</li>
+					<li>
+						<a href="termine"><i class="glyphicon glyphicon-calendar"></i> Termine</a>
+					</li>
+					<hr>
+					<li>
+						<a href="#" data-toggle="modal" data-target="#modalAusloggen"><i class="glyphicon glyphicon-off"></i> Ausloggen</a>
+					</li>
+				</nav>
+			</div>
+			<script>
+				window.addEventListener("DOMContentLoaded", function() {
+					$("#menu-toggle").click(function(e) {
+						e.preventDefault();
+						$("#wrapper").toggleClass("active");
+					});
+				});
+			</script>
+			<!-- Modal-Dialog zum Ausloggen -->
+			<div class="modal fade" id="modalAusloggen">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+							<h4 class="modal-title">Ausloggen</h4>
+						</div>
+						<div class="modal-body">
+							<p>Wollen Sie sich wirklich ausloggen?</p>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
+							<a href="ausloggen" class="btn btn-primary">Ausloggen</a>
+						</div>
+					</div><!-- /.modal-content -->
+				</div><!-- /.modal-dialog -->
+			</div><!-- /.modal -->
             @show
             <div id="page-content-wrapper">
                 <div class="content-header">
                     <h1>
-                    <a href="#"><a id="menu-toggle" href="#" class="btn btn-default"><i class="icon-reorder"></i></a>
-                    @section('title')
-                        Titel
-                    @show
+						<a href="#"><a id="menu-toggle" href="#" class="btn btn-default"><i class="icon-reorder"></i></a>
+							@section('title')
+							Titel
+							@show
                     </h1>
                 </div>
                 <!-- Der ganze Inhalt hier -->
                 <div class="page-content inset">
-                     @yield('content')
+					@yield('content')
                 </div>
             </div>
         </div>
-       
+
         <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
         <link rel="stylesheet" href="stylesheets/main.css">
         <link rel="stylesheet" href="stylesheets/simple-sidebar.css">

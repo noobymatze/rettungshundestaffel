43aee6f4adaadabead0726cb11f43cee9e564f8e
Eugen Feit <eugen.feit@googlemail.com>
2014-07-07 20:24:52 +0200

    Detailansicht

--- app/extensions/blade.php
+++ app/extensions/blade.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Erweitert die templates um einen helper, der für CSS 
+ * Erweitert die Templates um einen helper, der für CSS 
  * Fehlerklassen auf die folgende Weise benutzt werden kann:
  *
  * <div class="@hasError('email')"></div>
@@ -18,5 +18,20 @@ Blade::extend(function($view, $compiler)
 {
     $pattern = $compiler->createMatcher('hasError');
     return preg_replace($pattern, '<?php echo $errors->has($2) ? "has-error" : ""; ?>', $view);
+});
+
+/**
+ * Lässt in den Templates ein @ifstaffelleitung Statement benutzen. Damit kann man
+ * überprüfen, ob der aktuell eingeloggte Benutzer ein Admin (Staffelleitung) ist, um bestimmte Elemente
+ * nur für Admins sichtbar zu machen.
+ * 
+ * z. B. wird folgender Button nur gerendert, wenn man Staffelleitung ist:
+ * @ifstafelleitung
+ *		<button>Löschen</button>
+ * @endif
+ */
+Blade::extend(function($view, $compiler) 
+{
+    return str_replace("@ifstaffelleitung", '<?php if(Auth::user()->rolle == "Staffelleitung"): ?>', $view);
 });
 

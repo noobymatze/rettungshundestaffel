6985e599a55b698404ba721f08196482a48c93c0
J.H <ekip@gmx.de>
2014-07-19 11:54:39 +0200

    Mobile: CSS Dateien ausgelagert.

--- app/views/mitglieder/mobile/mitglied.blade.php
+++ app/views/mitglieder/mobile/mitglied.blade.php
@@ -1,87 +1,13 @@
 @extends('layouts.mobile-navigation')
 
+@section('css')
+<link rel="stylesheet" href="{{ URL::asset('stylesheets/mobile/mitgliedprofil.css') }}">
+@stop
+
 @section('header-center')
 <h1>{{{ $mitglied->vollerName() }}}</h1>
 @stop
 @section('content')
-<style>
-	.hinweis-paragraph {
-		font-size: 0.9em;
-		color: #333;
-		margin-left: 0.3em;
-	}
-	.einzug-links {
-		margin-left: 0.3em;
-	}
-	img.image-left {
-		width: 8em;
-		height: 8em;
-		position: absolute;
-		left: 0;
-		top: 0;
-	}
-	.info-div p {
-		margin: 0;
-		margin-bottom: 0.3em;
-		padding: 0;
-		color: #333;
-		font-size: 1em;
-	}
-	.info-div h2 {
-		font-size: 0.75em;
-		color:#666;
-		margin: 0 0;
-		padding: 0 auto;
-	}
-	.info-div h1 {
-		font-size: 0.9em;
-		color: #333;
-		margin: 0.3em 0;
-	}
-	.info-div {
-		font-style: normal;
-	}
-	.first-row, .hund-info-div {
-		position: relative;
-		padding: 0em 0;
-		padding-left: 8.5em;
-		/* height: 8em; */
-		border-bottom: #ddd solid 1px;
-		min-height: 8em;
-		overflow: hidden;
-	}
-	.hunde-div>h1 {
-		padding-left: 0.5em;
-	}
-	.kontakt-div>div {
-		position: relative;
-		padding-right: 3.5em;
-		overflow-wrap:break-word;
-	}
-	.kontakt-div>div i {
-		cursor: pointer;
-		right: .5625em;
-		font-size: 1em;
-		top: 50%;
-		margin-top: -1em;
-		position: absolute;
-	}
-	.kontakt-div>div>a {
-		text-decoration: none;
-		color: black;
-	}
-	span.geprueft-circle {
-		width: auto;
-		background-color: green;
-	}
-	.hund-div, .first-row {
-		border-bottom: #666 solid 2px;
-	}
-	.sucharten-div>* {
-		padding-left: 0.5em;
-	}
-</style>
-
 <div class="first-row">
 	<img class="pure-img image-left" src="{{{ $mitglied->profilbild() }}}">
 	<address class="info-div kontakt-div">

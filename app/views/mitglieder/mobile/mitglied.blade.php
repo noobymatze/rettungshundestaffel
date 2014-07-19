f16be68f9ec7bddd37d9b004ab071cf567ba64ac
J.H <ekip@gmx.de>
2014-07-19 11:37:20 +0200

    Mobile: E-Mail Feld zur Profilseite hinzugefügt.

--- app/views/mitglieder/mobile/mitglied.blade.php
+++ app/views/mitglieder/mobile/mitglied.blade.php
@@ -5,140 +5,6 @@
 @stop
 @section('content')
 <style>
-	form.search-form {
-		position: relative;
-		padding-right: 5em;
-		height: 3em;
-	}
-	.search-form>input[type="submit"], #search-button {
-		padding-left: 0.8em;
-		position: absolute;
-		right: 0;
-		top: 0;
-		width: 5em;
-		height: 3em;
-	}
-	@media only screen and (max-width: 480px) {
-		#search-button {
-			margin: 0;
-		}
-	}
-
-	.search-form>input[type="text"] {
-		margin-right: 5em;
-		height: 3.75em;
-		width: 100%;
-	}
-	.user-list>li {
-		overflow: visible;
-		display: block;
-		position: relative;
-		overflow: visible;
-	}
-	.user-list>li>a {
-		display: block;
-		position: relative;
-		overflow: hidden;
-		margin: 0;
-		padding: 0em 3.5em 0em 4.9em;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-	}
-	.user-list>li>a>i {
-		right: .5625em;
-		font-size: 1em;
-		top: 50%;
-		margin-top: -1em;
-		position: absolute;
-	}
-	.user-list>li>a>img {
-		position: absolute;
-		max-height: 4em;
-		max-width: 4em;
-		left: 0;
-		top: 0;
-	}
-	.user-list>li>a>.img-overlay {
-		position: absolute;
-		left: 0;
-		bottom: 0;
-		width: 4em;
-		height: .8em;
-		background-color: rgba(255, 255, 255, 0.8);
-	}
-	.user-list>li>a>h2 {
-		font-size: 1em;
-		margin: 0.5em 0;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-		color: #333;
-		text-shadow: 0 1px 0 #f3f3f3;
-	}
-	.user-list>li>a>p {
-		font-size: 0.9em;
-		margin: 0.5em 0;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-		color: #666;
-		text-shadow: 0 1px 0 #f3f3f3;
-	}
-	.user-list, .user-list>li {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-	}
-	.user-list>li {
-		background-color: #f6f6f6;
-		border-color: #ddd;
-		border-width: 1px 0;
-		border-style: solid;
-	}
-	.letter {
-		font-size: .9em;
-		margin: 0.4em 0em 0.4em 0.4em;
-		padding: 0;
-		color: #333;
-	}
-	.trail-circle, .flaeche-circle, .truemmer-circle, .geprueft-circle {
-		border-radius: 1em;
-		color: #FDFDFD;
-		height: 1em;
-		width: 1em;
-		display: inline-block;
-		font-size: .7em;
-		font-weight: bold;
-		padding: 0 0.1em;
-		margin-top: -0.2em;
-		margin-left: 0.0em;
-		padding-bottom: 0.2em;
-		vertical-align: middle;
-		text-align: center;
-		text-shadow: none;
-	}
-	.geprueft-circle {
-		padding-left: 0.5em;
-		padding-right: 0.5em;
-	}
-	.trail-circle {
-		background-color: rgb(197, 52, 52);
-	}
-	.flaeche-circle {
-		background-color: green;
-	}
-	.truemmer-circle {
-		background-color: blue;
-	}
-	.circle-list {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-		position: absolute;
-		right: 3.8em;
-		top: 0;
-		bottom: 0;
-	}
 	.hinweis-paragraph {
 		font-size: 0.9em;
 		color: #333;
@@ -189,13 +55,20 @@
 	}
 	.kontakt-div>div {
 		position: relative;
+		padding-right: 3.5em;
+		overflow-wrap:break-word;
 	}
-	.kontakt-div>div>i {
+	.kontakt-div>div i {
+		cursor: pointer;
 		right: .5625em;
 		font-size: 1em;
 		top: 50%;
 		margin-top: -1em;
 		position: absolute;
+	}
+	.kontakt-div>div>a {
+		text-decoration: none;
+		color: black;
 	}
 	span.geprueft-circle {
 		width: auto;
@@ -227,14 +100,14 @@
 				<i onclick="window.open('tel:{{{$mitglied->telefon}}}')" class="icon-phone"></i>
 			@endif
 		</div>
+		<div>
+			<h2>E-Mail</h2>
+			<p>{{{ $mitglied->email or '-' }}}</p>
+			@if (isset($mitglied->email))
+				<a href="mailto:{{{ $mitglied->email }}}"><i class="icon-phone"></i></a>
+			@endif
+		</div>
 	</address>
-	<!--<address class="info-div">
-		<h1>Adresse</h1>
-		<h2>Straße</h2>
-		<p>Landweg 14</p>
-		<h2>Wohnort</h2>
-		<p>24939 Flensburg-asdfsafasdfsadfasdf</p>
-	</address>-->
 </div>
 @if (count($mitglied->Hunde) > 0)
 <div class="info-div hunde-div">
@@ -268,6 +141,6 @@
 	@endforeach
 </div>
 @else
-	<p>{{{ $mitglied->vollerName() }}} hat bisher keine Hunde eingetragen.</p>
+	<p class="einzug-links">{{{ $mitglied->vollerName() }}} hat bisher keine Hunde eingetragen.</p>
 @endif
 @stop
